44547eea46134d8be6eb22bd71e57733c15cb794
A <158820696+abdullahnazir099@users.noreply.github.com>
2024-05-22 14:34:36 +0500

    Update page.tsx

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -5,7 +5,7 @@ import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription";
 
 const SettingsPage = async () => {
-  const isPro = await checkSubscription();
+  //const isPro = await checkSubscription();
 
   return ( 
     <div>
@@ -20,7 +20,7 @@ const SettingsPage = async () => {
         <div className="text-muted-foreground text-sm">
           {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
         </div>
-        <SubscriptionButton isPro={isPro} />
+        //<SubscriptionButton isPro={isPro} />
       </div>
     </div>
    );
