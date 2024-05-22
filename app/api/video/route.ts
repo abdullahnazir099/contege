f1a57d827817b263488a1e645786b746c2805bf2
A <158820696+abdullahnazir099@users.noreply.github.com>
2024-05-22 14:55:23 +0500

    Update route.ts

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -22,9 +22,7 @@ export async function POST(
       return new NextResponse("Prompt is required", { status: 400 });
     }
 
-    if (!freeTrial && !isPro) {
-      return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
-    }
+  
 
     const response = await replicate.run(
       "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
